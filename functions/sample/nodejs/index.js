f8289b02829d2ff7a206d822a7f9eaab318dfec4
Angelica Gardner <23157778+angelicagardner@users.noreply.github.com>
2022-07-31 15:33:32 +0200

    update cloudant db function

--- functions/sample/nodejs/index.js
+++ functions/sample/nodejs/index.js
@@ -6,22 +6,23 @@ const { CloudantV1 } = require('@ibm-cloud/cloudant');
 const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
 function main(params) {
+  const cloudant = Cloudant({
+    url: params.COUCH_URL,
+    plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
+});
 
-  const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
-  const cloudant = CloudantV1.newInstance({
-      authenticator: authenticator
-  });
-  cloudant.setServiceUrl(params.COUCH_URL);
-
-  let dbList = getDbs(cloudant);
-  return { dbs: dbList };
+  let dbListPromise = getDbs(cloudant);
+  return dbListPromise;
 }
 
 function getDbs(cloudant) {
-    cloudant.getAllDbs().then((body) => {
-        body.forEach((db) => {
-            dbList.push(db);
-        });
-    }).catch((err) => { console.log(err); });
+    return new Promise((resolve, reject) => {
+        cloudant.db.list()
+            .then(body => {
+                resolve({ dbs: body });
+            })
+            .catch(err => {
+                reject({ err: err });
+            });
+    });
 }
-
