7f6704d193479ef6e8286cb3349b9ceb902fc221
Angelica Gardner <23157778+angelicagardner@users.noreply.github.com>
2022-08-04 01:49:24 +0200

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -20,6 +20,8 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
+    
+    path('dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
 
