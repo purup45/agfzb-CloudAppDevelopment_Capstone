0b960fc0a01c2bf9abe2e0ad14b3aacc980170ee
Angelica Hjelm Gardner <angelicahjelmgardner@gmail.com>
2022-08-10 17:08:03 +0000

    add deployment

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,8 +27,11 @@ DEBUG = True
 APPEND_SLASH = True
 
 ALLOWED_HOSTS = ["localhost", "localhost:8000",
-                 "angelicagard.eu-gb.mybluemix.net"]
+                 "angelicagard.eu-gb.mybluemix.net", 
+                 "angelicagard-8000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
 
+CSRF_TRUSTED_ORIGINS = ['https://angelicagard-8000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/',
+                        "http://angelicagard.eu-gb.mybluemix.net",'https://*.127.0.0.1']
 
 # Application definition
 
