4bcd1cfa9cae6d4ab973b00aa4185d7cf4872763
Angelica Gardner <23157778+angelicagardner@users.noreply.github.com>
2022-08-10 19:44:38 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -40,7 +40,27 @@ def logout_request(request):
 
 
 def registration_request(request):
-    return render(request, 'djangoapp/registration.html')
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html')
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.error("New user")
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            login(request, user)
+            return redirect("djangoapp:index")
+        else:
+            context['message'] = "User already exists."
+            return render(request, 'djangoapp/registration.html', context)
 
 
 def get_dealerships(request):
